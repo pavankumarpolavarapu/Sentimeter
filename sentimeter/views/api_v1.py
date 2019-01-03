e92e1df7a9d92239348e2f155fc76a6b10a035ff
Pavan Kumar Polavarapu <pavan.vnr@gmail.com>
2019-01-02 19:50:17 -0500

    003 - Added Private Assistant Dialogflow Webhook Module

--- sentimeter/views/api_v1.py
+++ sentimeter/views/api_v1.py
@@ -5,7 +5,7 @@ from flask import jsonify, request, render_template, url_for, Response
 from sentimeter import app, logger
 from sentimeter.helpers.helpers import decorator_function
 from sentimeter.helpers.encryption import get_key, encode_data, decode_data
-
+import assistant.views 
 
 @app.route('/api/v1/test')
 def test():
